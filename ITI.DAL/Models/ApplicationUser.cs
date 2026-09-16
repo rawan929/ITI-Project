3ac0074e5211277d58c91fed2b09059708a825dd
samia-salama <samiasalama2005@gmail.com>
2026-09-16 17:22:17 +0300

    Add UserId FK to BloodBank and link with ApplicationUser

--- ITI.DAL/Models/ApplicationUser.cs
+++ ITI.DAL/Models/ApplicationUser.cs
@@ -7,17 +7,14 @@ namespace ITI.DAL.Models
 {
     public class ApplicationUser : IdentityUser<Guid>
     {
-        
         public string FullName { get; set; } = string.Empty;
-        
-        
-        
         public string City { get; set; } = string.Empty;
-        public string UserType { get; set; } = string.Empty; 
+        public string UserType { get; set; } = string.Empty;
         public bool IsActive { get; set; } = true;
 
         // Navigation Properties
         public Donor? Donor { get; set; }
         public Hospital? Hospital { get; set; }
+        public BloodBank? BloodBank { get; set; }
     }
 }
